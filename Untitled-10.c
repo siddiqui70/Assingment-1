1ec3eeb8ee81e83fa4f087765552e0952ef3139c
siddiqui70 <siddiquiumear@gmail.com>
2024-05-26 23:07:36 +0530

    Update Untitled-10.c

--- Untitled-10.c
+++ Untitled-10.c
@@ -5,14 +5,14 @@ int main() {
     float radius, volume;
     const float PI = 3.14159265359;
 
-    // Ask the user to enter the radius of the sphere
+   
     printf("Enter the radius of the sphere: ");
     scanf("%f", &radius);
 
-    // Calculate the volume of the sphere
+  
     volume = (4.0 / 3.0) * PI * pow(radius, 3);
 
-    // Display the result
+ 
     printf("Volume of the sphere: %.2f cubic units\n", volume);
 
     return 0;
